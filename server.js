8e462e656d4c30442bc6cc00ea4bb79a77290b57
Anastasia Organova <organova.nastya@gmail.com>
2025-03-14 00:14:44 +0300

    Добавлен pg-promise, подключение к БД перенесено в отдельный файл

--- server.js
+++ server.js
@@ -1,24 +1,9 @@
-require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const { Pool } = require("pg");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
-
-const pool = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-});
-
-pool
-  .connect()
-  .then(() => console.log("База данных подключена"))
-  .catch((err) => console.error("Ошибка подключения:", err));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Сервер запущен на порту ${PORT}`));
